adb7e5dbdcb0d01d6645338abd55488aba435e20
Yuki Ishizaki <ishiyuki405@icloud.com>
2020-03-28 21:44:15 +0900

    マイページへのアクセスを修正いたしました。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
-class UsersController < Application
+class UsersController < ApplicationController
   def show
     # @items = Item.where(user_id: current_user.id)
     @user = User.find(current_user.id)
-    
   end
 end
